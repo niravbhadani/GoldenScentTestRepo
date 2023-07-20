d30c7c9809cc740a1c6aae6a7497f741f2cc83eb
Nirav Bhadani <niravbhadani9@gmail.com>
2023-07-20 18:56:13 +0530

    Created RowData model

--- GoldenScentTest/Models/RowData.swift
+++ GoldenScentTest/Models/RowData.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+
+struct RowData: Decodable {
+    let marginLeft: String?
+    let marginRight: String?
+    let marginBottom: String?
+    let height: String?
+    let columns: [ColumnData]
+    
+    private enum CodingKeys: String, CodingKey {
+        case marginLeft = "row-margin-left"
+        case marginRight = "row-margin-right"
+        case marginBottom = "row-margin-bottom"
+        case height, columns
+    }
+}
