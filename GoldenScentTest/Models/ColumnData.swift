3c1d96327462f2038ff7d96341d960c314693400
Nirav Bhadani <niravbhadani9@gmail.com>
2023-07-20 19:08:24 +0530

    Created helping methods for ColumnData model

--- GoldenScentTest/Models/ColumnData.swift
+++ GoldenScentTest/Models/ColumnData.swift
@@ -5,7 +5,7 @@
 //  Created by Nirav Bhadani on 20/07/23.
 //
 
-import Foundation
+import UIKit
 
 enum ColumnType: String, Decodable {
     case text = "text"
@@ -31,5 +31,61 @@ struct ColumnData: Decodable {
         case fontColor = "font-color"
         case fontSize = "font-size"
         case font, background, slides
+    }
+    
+    //MARK: Methods
+    func getImageURL() -> URL? {
+        guard let urlStr = src
+        else {
+            return nil
+        }
+    
+        return URL(string: urlStr)
+    }
+    
+    func getTextAlignment() -> NSTextAlignment {
+        guard let alignment = textAlignment
+        else {
+            return .natural
+        }
+        
+        switch alignment {
+        case "letf":
+            return .left
+        case "right":
+            return .right
+        case "center":
+            return .center
+        default:
+            return.natural
+        }
+    }
+    
+    func getFont() -> UIFont {
+        guard let fontName = font,
+              let fontSize = fontSize
+        else {
+            return .systemFont(ofSize: 13)
+        }
+        
+        let font = UIFont(name: fontName, size: fontSize.convetToCGFlot() ?? 13)
+        return font ?? .systemFont(ofSize: 13)
+    }
+    
+    func getFontColor() -> UIColor {
+        guard let fontColor = fontColor
+        else { return .black }
+        
+        return fontColor.convertToUIColor() ?? .black
+    }
+    
+    func getBackgroundColor() -> UIColor {
+        guard let backgroundDict = background,
+              let colorString = backgroundDict["color"]
+        else {
+            return .white
+        }
+        
+        return colorString.convertToUIColor() ?? .white
     }
 }
