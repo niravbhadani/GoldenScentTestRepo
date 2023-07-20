aae3d81f50ffe58cde4a47409f4f8c3baf529007
Nirav Bhadani <niravbhadani9@gmail.com>
2023-07-20 20:07:22 +0530

    Updated hex string to Color converter method.

--- GoldenScentTest/Helpers/ExtentionHelpers.swift
+++ GoldenScentTest/Helpers/ExtentionHelpers.swift
@@ -18,26 +18,24 @@ extension String {
     }
     
     func convertToUIColor() -> UIColor? {
-        let r, g, b, a: CGFloat
+        var hexString = self.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
 
-        if self.hasPrefix("#") {
-            let start = self.index(self.startIndex, offsetBy: 1)
-            let hexColor = String(self[start...])
-            
-            if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-                
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-                    
-                    return UIColor(red: r, green: g, blue: b, alpha: a)
-                }
-            }
+        if hexString.hasPrefix("#") {
+            hexString.remove(at: hexString.startIndex)
         }
-        return nil
+
+        guard hexString.count == 6 else {
+            return nil
+        }
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexString).scanHexInt64(&rgbValue)
+
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgbValue & 0x0000FF) / 255.0
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
+        
 }
