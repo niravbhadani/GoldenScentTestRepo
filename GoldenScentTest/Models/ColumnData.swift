5beff6728a00b8d4e2b276349faac3a2e05d50e4
Nirav Bhadani <niravbhadani9@gmail.com>
2023-07-20 18:53:26 +0530

    Created ColumnData and ColumnType models

--- GoldenScentTest/Models/ColumnData.swift
+++ GoldenScentTest/Models/ColumnData.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+enum ColumnType: String, Decodable {
+    case text = "text"
+    case image = "image"
+    case slider = "custom-slider"
+}
+
+
+struct ColumnData: Decodable {
+    let type: ColumnType
+    let src: String?
+    let slides: [SliderData]?
+    let content: String?
+    let textAlignment: String?
+    let fontColor: String?
+    let fontSize: String?
+    let font: String?
+    let background: [String: String]?
+    
+    private enum CodingKeys: String, CodingKey {
+        case type, src, content
+        case textAlignment = "text-align"
+        case fontColor = "font-color"
+        case fontSize = "font-size"
+        case font, background, slides
+    }
+}
